5dc43df8e1d2e2f3fa5e94e1f136d2dca3a334c5
Aris Tzoumas <atzoumas@rudderstack.com>
2022-05-24 23:57:45 +0300

    initial commit

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # kapacitor-join
+
+    cd /var/lib/kapacitor/tasks
+    kapacitor define alert -tick alert.tick
+    kapacitor list tasks
+    kapacitor show alert
+    kapacitor enable alert
---
+++ send-metrics.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+INPUTNUM=$1
+OUTPUTNUM=$2
+PAYLOAD_FILE="batch.json"
+if [ "${RSOURCES:-false}" = "true" ]; then
+  PAYLOAD_FILE="batch-sources.json"
+fi
+
+dir=$( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P )
+
+getJson () {
+    sed "s/ANON_ID/$(echo $RANDOM | md5 | head -c 20)/" ${PAYLOAD_FILE}
+}
+doPost () {
+  count=$1
+  direction=$2
+  curl -i -XPOST 'http://localhost:9992/write?db=metrics' --data-binary "jobsdb_events,dir=$direction value=$count"
+}
+
+
+
+doPost $INPUTNUM "in" &
+if [ "$OUTPUTNUM" -gt "0" ]; then
+  doPost $OUTPUTNUM "out" &    
+fi
+
+
+wait
